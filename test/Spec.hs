61d8264e9d6ce8e3c4014a3c596f868283b21eae
Thomas BAGREL <tomsb07@gmail.com>
2021-09-23 17:15:59 +0200

    test CI 1

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Coords
+  ( addCoord,
+  )
+import Test.Hspec
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Coords.addCoords" $ do
+    it "adds coordinates of both points" $ do
+      addCoord (1, 2) (3, 4) `shouldBe` ((4, 6) :: (Int, Int))
